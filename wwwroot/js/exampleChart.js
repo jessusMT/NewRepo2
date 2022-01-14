5e4485e094c044ac68e478fdd476e5b007ba91ec
Jesus Jimenez Rendon <jesusJimenezRE777@outlook.com>
2022-01-13 20:49:58 -0600

    proyecto

---
+++ wwwroot/js/exampleChart.js
@@ -0,0 +1,24 @@
+﻿var ctx = document.getElementById('myChart2').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets: [{
+            label: 'apples',
+            data: [12, 19, 3, 17, 6, 3, 7],
+            backgroundColor: "rgba(153,255,51,0.4)"
+        }, {
+            label: 'oranges',
+            data: [2, 29, 5, 5, 2, 3, 10],
+            backgroundColor: "rgba(255,153,0,0.4)"
+        }]
+    },
+    options: {
+        animation: {
+            onComplete: function () {
+               
+                window.JSREPORT_READY_TO_START = true
+            }
+        }
+    }
+});
